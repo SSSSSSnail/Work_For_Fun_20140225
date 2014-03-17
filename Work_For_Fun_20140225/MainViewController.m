a724a047310a54bb5478cae8dc2d40a8792f6485
Snail <warlocke@163.com>
2014-03-17 15:36:44 +0800

    同步患者情况

--- Work_For_Fun_20140225/MainViewController.m
+++ Work_For_Fun_20140225/MainViewController.m
@@ -175,8 +175,8 @@ static float const DETAILVIEWIDTH = 872.0f;
     [super viewWillAppear:animated];
 #warning TODO: 加载旧数据并赋值
     GInstance().globalData.currentIndex = 5;
-    [_hzqkM2ViewController reloadViewDataForR2];
     GInstance().globalData.r2Type = M1;
+    [_hzqkM2ViewController reloadViewDataForR2];
 
     [self refreshButtonAndView:GInstance().globalData.currentIndex];
 }
--- Work_For_Fun_20140225/mainScorllViews/HZQKViewController.m
+++ Work_For_Fun_20140225/mainScorllViews/HZQKViewController.m
@@ -8,10 +8,12 @@
 
 #import "HZQKViewController.h"
 
-@interface HZQKViewController ()
+@interface HZQKViewController ()<UITableViewDataSource>
 
 - (IBAction)confirmClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UILabel *dateTimeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
+@property (weak, nonatomic) IBOutlet UITableView *bshgTable;
 
 @end
 
@@ -30,6 +32,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    UILabel *lcjcHeaderLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 200.0f, 40.0f)];
+    lcjcHeaderLabel.textAlignment = NSTextAlignmentCenter;
+    lcjcHeaderLabel.text = @"病史回顾";
+    lcjcHeaderLabel.font = [UIFont miscrosoftYaHeiFontWithSize:22.0f];
+    lcjcHeaderLabel.textColor = [UIColor colorWithRed:2.0f/255 green:128.0f/255 blue:127.0f/255 alpha:1];
+    _bshgTable.tableHeaderView = lcjcHeaderLabel;
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,6 +56,8 @@
 #pragma mark - Reload Data
 - (void)reloadViewDataForR2
 {
+    _bgImageView.hidden = YES;
+    _bshgTable.hidden = NO;
 
 }
 
@@ -56,4 +66,27 @@
         [_scrollViewDelegate didClickConfirmButton:sender];
     }
 }
+
+#pragma mark - TableView DataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"oneLabelCell"];
+    cell.textLabel.font = [UIFont miscrosoftYaHeiFontWithSize:14.0f];
+    cell.textLabel.textColor = [UIColor colorWithRed:2.0f/255 green:128.0f/255 blue:127.0f/255 alpha:1];
+    cell.textLabel.text = @"测试";
+
+    return cell;
+}
+
+#pragma mark - TableView Delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40.0f;
+}
+
 @end
